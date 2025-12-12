4bfd59e90922e42af7a8ad6e722c64a0626afa5d
Nguyễn Thanh Tân <me@tozydev.id.vn>
2025-12-12 18:25:48 +0700

    chore: update release script

--- release.sh
+++ release.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 
+read_version_from_package_json() {
+  bun -p "require('./package.json').version"
+}
+
+VERSION=$(read_version_from_package_json)
+
+echo "Releasing version $VERSION"
+
+# Build the project
 bun run build
 
-VERSION=$(bun -p "require('./package.json').version")
+# Create git tag and GitHub release
+git tag v"$VERSION" --sign --message "$VERSION"
+gh release create v"$VERSION" --notes-from-tag -t "$VERSION" --target main ./dist/*
 
-gh release create v"$VERSION" --notes-from-tag -t "$VERSION" ./dist/*
+# Bump to the next version (patch)
+bun pm version patch --no-git-tag-version
+git add package.json public/manifest.json versions.json
+git commit -m "chore(version): bump to v$(read_version_from_package_json)"
+git push origin main
+echo "Released version $VERSION"
